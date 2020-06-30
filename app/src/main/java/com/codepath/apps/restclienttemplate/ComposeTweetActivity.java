47757b54fb068ac953beec102b5f9f238d502512
Matthew Lacayo <mattlacayo@berkeley.edu>
2020-06-30 16:39:29 -0700

    Added tweet post functionality to post button

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeTweetActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeTweetActivity.java
@@ -4,21 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import okhttp3.Headers;
+
 public class ComposeTweetActivity extends AppCompatActivity {
 
+    private static final String TAG = "ComposeTweetActivity";
     private TextView etBody;
     private Button btSubmit;
+
+    private TwitterClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose_tweet);
+
+        client = TwitterApplication.getRestClient(this);
 
         bindElements();
         setupPostButton();
@@ -49,10 +59,22 @@ public class ComposeTweetActivity extends AppCompatActivity {
                     Toast.makeText(ComposeTweetActivity.this,
                             "Sorry, your tweet is too long",
                             Toast.LENGTH_SHORT).show();
+                } else {
+                    Log.i(TAG, "Posting Tweet...");
+                    client.postTweet(tweetBody, new JsonHttpResponseHandler() {
+                        @Override
+                        public void onSuccess(int statusCode, Headers headers, JSON json) {
+                            Log.i(TAG, "Successfully posted tweet");
+                        }
+
+                        @Override
+                        public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                            Log.e(TAG, "Failed to post tweet");
+                        }
+                    });
                 }
 
-                hideKeyboard();
-                Toast.makeText(ComposeTweetActivity.this, tweetBody, Toast.LENGTH_SHORT).show();
+
             }
         });
     }
